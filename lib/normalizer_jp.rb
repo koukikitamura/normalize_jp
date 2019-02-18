967ecd6915a9004a22d74b22d8fc604164dfd7a1
koukikitamura <kitamura3116@gmail.com>
2019-02-18 12:22:28 +0900

    Initialize project

---
+++ lib/normalizer_jp.rb
@@ -0,0 +1,6 @@
+require "normalizer_jp/version"
+
+module NormalizerJp
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/normalizer_jp/version.rb
@@ -0,0 +1,3 @@
+module NormalizerJp
+  VERSION = "0.1.0"
+end
