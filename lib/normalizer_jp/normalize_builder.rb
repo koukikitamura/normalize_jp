77dc74c3ef69deb9c6a5599d0cc6a35435c57e79
koukikitamura <kitamura3116@gmail.com>
2019-02-19 14:08:26 +0900

    rename variable name

--- lib/normalizer_jp/normalize_builder.rb
+++ lib/normalizer_jp/normalize_builder.rb
@@ -1,7 +1,7 @@
 class NormalizerBuilder < Module
-  def initialize(attribute, uploader)
+  def initialize(attribute, normalizer)
     define_method "#{attribute}=" do |value|
-      normalized_value = uploader.call(value)
+      normalized_value = normalizer.call(value)
       super(normalized_value)
     end
   end
